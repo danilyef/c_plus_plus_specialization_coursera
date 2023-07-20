87244c142f88683d59821afa15aab54bf74ef3ae
Daniil Yefimov <daniil.yefimov@MacBook-Pro-von-Daniil.fritz.box>
2023-07-20 15:02:15 +0200

    added yellow_belt exercise

--- yellow_belt/1 week/block_measurement/block_measurement.cpp
+++ yellow_belt/1 week/block_measurement/block_measurement.cpp
@@ -1,26 +1,51 @@
-#include <iostream>
-#include <cstdint>
-#include <limits>
-using namespace std;
-
 
 /*
-Вычислите суммарную массу имеющих форму прямоугольного параллелепипеда бетонных блоков одинаковой плотности, но разного размера.
+Block Mass
+Calculate the total mass of rectangular parallelepiped concrete blocks of the same density but different sizes.
+
+Instructions:
+Assume that the dimensions of the blocks are measured in centimeters, the density is in grams per cubic centimeter, and the total mass is in grams. Therefore, the mass of each block can be computed as a simple product of the density and volume.
+
+Input Format:
+The first line contains two positive integers: N (the number of blocks) and R (the density of each block). Each of the following N lines contains three positive integers: W, H, D — the dimensions of each respective block.
+
+Guarantees:
+
+The number of blocks N does not exceed 10^5.
+The density of blocks R does not exceed 100.
+The dimensions of blocks W, H, D do not exceed 10^4.
+Example:
+Input
+2 14
+1 2 3
+30 40 50
+
+Output
+840084
 */
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdint>
+
+using namespace std;
 
 int main(){
+    int N;
+    u_int16_t R;
+    u_int64_t W,H,D;
 
-    int N,R,W,H,D;
-    uint64_t sum = 0;
+    u_int64_t overall_mass = 0;
+
     cin >> N >> R;
-    for(int i = 0; i < N ; ++i){
+
+    for(int i = 0; i < N;++i){
         cin >> W >> H >> D;
-        sum += static_cast<uint64_t>(W) * H * D;
+        overall_mass += (R * W * H * D);
     }
 
-    cout << sum * R << endl;
+    cout << overall_mass;
 
     return 0;
 }
-
