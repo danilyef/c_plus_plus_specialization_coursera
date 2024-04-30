894f6fe0347a29de91fc6afab7e7155a97f32e93
danilyef <daniil.yefimov92@gmail.com>
2024-04-30 08:13:13 +0200

    priority collection

---
+++ red_belt/4 week/priority_collection/README.md
@@ -0,0 +1,63 @@
+#### Programming Assignment: "Priority Container"
+
+Task Description
+Implement a template container PriorityCollection that allows storing objects of non-copyable type and performing the following operations with them:
+
+- add an element with zero priority;
+- add a set of elements with zero priorities;
+- increase the priority of a specified element by 1;
+- extract the element with the maximum priority from the container.
+
+Since adding an element involves moving it into the container, the method for increasing the priority cannot accept the element itself. Therefore, the method that adds elements should return some identifier for each of them, which can then be used to call other methods.
+
+The type of the identifier — PriorityCollection::Id — should be chosen by you. It should be easily copyable, for example, a number, pointer, or iterator. Examples of defining and using the Id type can be found in the solution template.
+
+Class Structure
+```cpp
+template <typename T>
+class PriorityCollection {
+public:
+  using Id = /* type used for identifiers */;
+
+  // Add an object with zero priority
+  // by moving it and return its identifier
+  Id Add(T object);
+
+  // Add all elements in the range [range_begin, range_end)
+  // by moving them, writing the issued identifiers
+  // to the range [ids_begin, ...)
+  template <typename ObjInputIt, typename IdOutputIt>
+  void Add(ObjInputIt range_begin, ObjInputIt range_end,
+           IdOutputIt ids_begin);
+
+  // Determine whether the identifier belongs to any
+  // object stored in the container
+  bool IsValid(Id id) const;
+
+  // Get the object by identifier
+  const T& Get(Id id) const;
+
+  // Increase the priority of the object by 1
+  void Promote(Id id);
+
+  // Get the object with the maximum priority and its priority
+  pair<const T&, int> GetMax() const;
+
+  // Similar to GetMax, but removes the element from the container
+  pair<T, int> PopMax();
+
+private:
+  // Private fields and methods
+};
+```
+
+Guarantees
+You can rely on the fact that for elements of type T, move and comparison operations (==, !=, <) are defined. All operations on elements have constant complexity.
+
+It is guaranteed that all added elements are distinct, but an element that has been added and then extracted may be added again.
+
+The Get and Promote methods are always called with valid identifiers according to the IsValid method.
+
+The GetMax and PopMax methods are called only when there are elements in the container.
+
+If there are multiple elements with the same maximum priority, the one that was added later than the others should be selected and extracted.
---
+++ red_belt/4 week/priority_collection/priority_collection_2.cpp
@@ -0,0 +1,127 @@
+#include "test_runner.h"
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <set>
+#include <utility>
+#include <vector>
+#include <typeinfo>
+
+using namespace std;
+
+
+template <typename T>
+class PriorityCollection {
+public:
+    
+    using Id = size_t;
+    
+    // Add an object with zero priority
+   // by moving it and return its identifier
+    Id Add(T object){
+        id_objects.emplace(0,id);
+        objects.emplace_back(std::move(object),0);
+        return Id(id++);
+    };
+
+
+   // Add all elements in the range [range_begin, range_end)
+   // by moving them, writing the issued identifiers
+   // to the range [ids_begin, ...)
+    
+    template <typename ObjInputIt, typename IdOutputIt>
+    void Add(ObjInputIt range_begin, ObjInputIt range_end, IdOutputIt ids_begin){
+        while (range_begin != range_end){
+            *ids_begin++ = Add(std::move(*range_begin++));
+        } 
+
+    };
+    
+    // Determine whether the identifier belongs to any
+    // object stored in the container
+    bool IsValid(Id id) const {
+        return id < objects.size();
+    };
+
+    // Get the object by identifier
+    const T& Get(Id id) const{
+        return objects[id].first;
+    };
+
+    // Increase the priority of the object by 1
+    void Promote(Id id) {
+        id_objects.erase({objects[id].second,id});
+        objects[id].second++;
+        id_objects.insert(std::make_pair(objects[id].second,id));
+    };
+
+    // Get the object with the maximum priority and its priority
+    pair<const T&, int> GetMax() const {
+        auto id = id_objects.begin()->second;
+        return objects[id];
+    };
+
+
+    // Similar to GetMax, but removes the element from the container
+    pair<T, int> PopMax(){
+        auto id = id_objects.begin()->second;
+        id_objects.erase(id_objects.begin());
+        auto result = std::move(objects[id]);
+        objects[id] = {T(), -1};
+        return result;
+    };
+
+private:
+    int id = 0;
+    vector<pair<T,int>> objects;
+    set<pair<int,Id>, std::greater<>> id_objects;
+};
+
+
+class StringNonCopyable : public string {
+public:
+    using string::string;  
+    StringNonCopyable(const StringNonCopyable&) = delete;
+    StringNonCopyable(StringNonCopyable&&) = default;
+    StringNonCopyable& operator=(const StringNonCopyable&) = delete;
+    StringNonCopyable& operator=(StringNonCopyable&&) = default;
+};
+
+
+void TestNoCopy() {
+    PriorityCollection<StringNonCopyable> strings;
+    const auto white_id = strings.Add("white");
+    const auto yellow_id = strings.Add("yellow");
+    const auto red_id = strings.Add("red");
+
+    strings.Promote(yellow_id);
+    for (int i = 0; i < 2; ++i) {
+    strings.Promote(red_id);
+    }
+    strings.Promote(yellow_id);
+    {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "red");
+    ASSERT_EQUAL(item.second, 2);
+    }
+    {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "yellow");
+    ASSERT_EQUAL(item.second, 2);
+    }
+    {
+    const auto item = strings.PopMax();
+    ASSERT_EQUAL(item.first, "white");
+    ASSERT_EQUAL(item.second, 0);
+    }
+}
+
+
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, TestNoCopy);
+   
+    return 0;
+}
+
