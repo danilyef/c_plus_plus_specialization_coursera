cae5b747f2c4fa046bd54f23bde719d21b89e812
danilyef <daniil.yefimov92@gmail.com>
2024-05-07 19:56:58 +0200

    template sync

---
+++ red_belt/5 week/synchronized_template/README.md
@@ -0,0 +1,37 @@
+#### Programming Task: "Synchronized Template"
+
+We have seen multiple times that the lack of synchronization of access by multiple threads to the same data can fatally affect the program's operation. Therefore, it is very important not to forget to acquire a mutex when accessing shared data. But if we have a large project where access to shared data is done from many places in the code, it's not that difficult to forget to do so when making changes.
+
+To protect ourselves from such forgetfulness, we can write a special wrapper around the data, which will provide access to them only under a mutex. In this task, you need to develop such a wrapper - a class template named Synchronized with the following interface:
+
+```cpp
+template <typename T>
+class Synchronized {
+public:
+  explicit Synchronized(T initial = T());
+
+  struct Access {
+    T& ref_to_value;
+  };
+
+  Access GetAccess();
+private:
+  T value;
+};
+```
+
+The Access type should be a structure with a field `T& ref_to_value`, referring to the `value` field of the `Synchronized<T>` class. In addition, an object of the Access class should behave similarly to the `lock_guard<mutex>` class: it acquires the mutex in its constructor and releases it in its destructor. For example:
+
+```cpp
+Synchronized<int> s_int;
+{
+  auto access = s_int.GetAccess();
+  // The critical section starts here and continues until the end of the block
+  access.ref_to_value = 5;
+}
+ASSERT_EQUAL(s_int.GetAccess().value, 5);
+```
+
+You can (and should) add necessary fields to the Access class and the Synchronized template. For more detailed examples of using the Synchronized template, see the solution template.
+
+As you can see, the only way to access the `value` field of the `Synchronized<T>` class is to access the `ref_to_value` field of the `Access` class, which guarantees that this access is performed under a mutex. Thus, if you wrap shared data in the Synchronized template, you will never forget to acquire the mutex when accessing them.
---
+++ red_belt/5 week/synchronized_template/synchronized.cpp
@@ -0,0 +1,106 @@
+#include "test_runner.h"
+
+#include <numeric>
+#include <vector>
+#include <string>
+#include <future>
+#include <mutex>
+#include <queue>
+#include <thread>
+using namespace std;
+
+
+template <typename T>
+class Synchronized {
+public:
+	explicit Synchronized(T initial = T()): value(std::move(initial)){};
+
+	struct Access {
+		Access(T& value,mutex& m): ref_to_value(value), lock(m){};
+		~Access(){};
+		T& ref_to_value;
+		lock_guard<mutex> lock;
+	};
+
+	Access GetAccess(){
+		return {value,m};
+	};
+	
+private:
+	T value;
+	mutex m;
+};
+
+void TestConcurrentUpdate() {
+	Synchronized<string> common_string;
+
+	const size_t add_count = 50000;
+	auto updater = [&common_string, add_count] {
+	for (size_t i = 0; i < add_count; ++i) {
+		auto access = common_string.GetAccess();
+		access.ref_to_value += 'a';
+		}
+	};
+
+	auto f1 = async(updater);
+	auto f2 = async(updater);
+
+	f1.get();
+	f2.get();
+
+	ASSERT_EQUAL(common_string.GetAccess().ref_to_value.size(), 2 * add_count);
+	}
+
+vector<int> Consume(Synchronized<deque<int>>& common_queue) {
+	vector<int> got;
+
+	for (;;) {
+		deque<int> q;
+
+		{
+			// We intentionally enclosed these two lines in curly braces to
+			// reduce the size of the critical section. The consumer thread acquires
+			// the mutex, moves all the contents of the shared queue into its
+			// local variable, and releases the mutex. After that, it processes
+			// the objects in the queue outside the critical section, allowing
+			// the producer thread to concurrently place new objects into the queue.
+			//
+			// The size of the critical section significantly affects the performance
+			// of multithreaded programs.
+			auto access = common_queue.GetAccess();
+			q = std::move(access.ref_to_value);
+		}
+
+		for (int item : q) {
+			if (item > 0) {
+			got.push_back(item);
+			} else {
+			return got;
+			}
+		}
+	}
+}
+
+
+void TestProducerConsumer() {
+	Synchronized<deque<int>> common_queue;
+
+	auto consumer = async(Consume, ref(common_queue));
+
+	const size_t item_count = 100000;
+	for (size_t i = 1; i <= item_count; ++i) {
+	common_queue.GetAccess().ref_to_value.push_back(i);
+	}
+	common_queue.GetAccess().ref_to_value.push_back(-1);
+
+	vector<int> expected(item_count);
+	iota(begin(expected), end(expected), 1);
+	ASSERT_EQUAL(consumer.get(), expected);
+}
+
+
+int main() {
+	TestRunner tr;
+	RUN_TEST(tr, TestConcurrentUpdate);
+	RUN_TEST(tr, TestProducerConsumer);
+}
