ce06a60f1efdb4d068e82e4f4dba6b3cb01dd022
danilyef <daniil.yefimov92@gmail.com>
2024-06-27 20:20:19 +0200

    added 1 exercise brown belt

---
+++ brown_belt/1 week/hash_table/README.md
@@ -0,0 +1,30 @@
+#### Programming Task "Hash Table"
+
+Now that you know how unordered containers are structured internally, you are asked to implement a simplified version of a hash table yourself. Implement the HashSet template:
+
+```cpp
+template <typename Type, typename Hasher>
+class HashSet {
+public:
+  using BucketList = forward_list<Type>;
+
+public:
+  explicit HashSet(
+      size_t num_buckets,
+      const Hasher& hasher = {}
+  );
+
+  void Add(const Type& value);
+  bool Has(const Type& value) const ;
+  void Erase(const Type& value);
+  const BucketList& GetBucket(const Type& value) const;
+};
+```
+
+Requirements:
+
+- At any point in time, your hash table should have exactly `num_buckets` buckets. Rehashing is not required.
+- The `Add` method should not change the contents of the table if an element equal to the given one already exists in the table.
+- The `Has` method should return `true` if and only if an element equal to the given one is in the table.
+- The `Erase` method should remove an element equal to the given one from the table if it exists; otherwise, it should not change the contents of the table.
+- The `GetBucket` method should return a reference to the bucket that would contain the given element, regardless of whether it is in the table or not.
---
+++ brown_belt/1 week/hash_table/hash_set.cpp
@@ -0,0 +1,160 @@
+#include "test_runner.h"
+
+#include <forward_list>
+#include <iterator>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+template <typename Type, typename Hasher>
+class HashSet {
+public:
+  using BucketList = forward_list<Type>;
+
+public:
+  explicit HashSet(
+      size_t num_buckets,
+      const Hasher& hasher = {}
+  );
+
+  void Add(const Type& value);
+  bool Has(const Type& value) const;
+  void Erase(const Type& value);
+  const BucketList& GetBucket(const Type& value) const;
+
+private:
+  unordered_map<size_t,BucketList> hash_table;
+  const Hasher& hasher;
+  size_t num_buckets;
+};
+
+struct IntHasher {
+  size_t operator()(int value) const {
+    return value;
+  }
+};
+
+template <typename Type, typename Hasher>
+HashSet<Type,Hasher>::HashSet(size_t num_buckets,const Hasher& hasher): num_buckets(num_buckets),hash_table(num_buckets),hasher(hasher){
+  for(int i = 0; i < num_buckets; ++i){
+    hash_table[i];
+  }
+
+};
+
+template <typename Type, typename Hasher>
+bool HashSet<Type,Hasher>::Has(const Type& value) const {
+  size_t bucket_value = hasher(value) % num_buckets;
+  return std::find(hash_table.at(bucket_value).begin(), hash_table.at(bucket_value).end(), value) != std::end(hash_table.at(bucket_value));
+  
+
+};
+ 
+
+template <typename Type, typename Hasher>
+void HashSet<Type,Hasher>::Add(const Type& value){
+  if(!Has(value)){
+    hash_table[hasher(value) % num_buckets].push_front(value);
+  }
+
+};
+
+
+
+template <typename Type, typename Hasher>
+void HashSet<Type,Hasher>::Erase(const Type& value) {
+  if(Has(value)){
+    hash_table[hasher(value) % num_buckets].remove(value);
+  }
+};
+
+
+template <typename Type, typename Hasher>
+const typename HashSet<Type, Hasher>::BucketList&
+HashSet<Type, Hasher>::GetBucket(const Type& value) const {
+  size_t bucket_value = hasher(value) % num_buckets;
+  return hash_table.at(bucket_value);
+};
+
+
+struct TestValue {
+  int value;
+
+  bool operator==(TestValue other) const {
+    return value / 2 == other.value / 2;
+  }
+};
+
+struct TestValueHasher {
+  size_t operator()(TestValue value) const {
+    return value.value / 2;
+  }
+};
+
+void TestSmoke() {
+  HashSet<int, IntHasher> hash_set(2);
+  hash_set.Add(3);
+  hash_set.Add(4);
+
+  ASSERT(hash_set.Has(3));
+  ASSERT(hash_set.Has(4));
+  ASSERT(!hash_set.Has(5));
+
+  hash_set.Erase(3);
+
+  ASSERT(!hash_set.Has(3));
+  ASSERT(hash_set.Has(4));
+  ASSERT(!hash_set.Has(5));
+
+  hash_set.Add(3);
+  hash_set.Add(5);
+
+  ASSERT(hash_set.Has(3));
+  ASSERT(hash_set.Has(4));
+  ASSERT(hash_set.Has(5));
+}
+
+void TestEmpty() {
+  HashSet<int, IntHasher> hash_set(10);
+  for (int value = 0; value < 10000; ++value) {
+    ASSERT(!hash_set.Has(value));
+  }
+}
+
+void TestIdempotency() {
+  HashSet<int, IntHasher> hash_set(10);
+  hash_set.Add(5);
+  ASSERT(hash_set.Has(5));
+  hash_set.Add(5);
+  ASSERT(hash_set.Has(5));
+  hash_set.Erase(5);
+  ASSERT(!hash_set.Has(5));
+  hash_set.Erase(5);
+  ASSERT(!hash_set.Has(5));
+}
+
+void TestEquivalence() {
+  HashSet<TestValue, TestValueHasher> hash_set(10);
+  hash_set.Add(TestValue{2});
+  hash_set.Add(TestValue{3});
+
+  ASSERT(hash_set.Has(TestValue{2}));
+  ASSERT(hash_set.Has(TestValue{3}));
+
+  const auto& bucket = hash_set.GetBucket(TestValue{2});
+  const auto& three_bucket = hash_set.GetBucket(TestValue{3});
+  ASSERT_EQUAL(&bucket, &three_bucket);
+
+  ASSERT_EQUAL(1, distance(begin(bucket), end(bucket)));
+  ASSERT_EQUAL(2, bucket.front().value);
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestSmoke);
+  RUN_TEST(tr, TestEmpty);
+  RUN_TEST(tr, TestIdempotency);
+  RUN_TEST(tr, TestEquivalence);
+  return 0;
+}
